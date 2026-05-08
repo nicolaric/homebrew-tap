c7429d5731e9a5183bca9e5b78a3c7f93e3718cf
Nicola Richli <nicola@akenza.io>
2026-05-08 04:35:52 +0000

    fix: add bottle root_url for GitHub Releases

--- Formula/minicode.rb
+++ Formula/minicode.rb
@@ -6,6 +6,7 @@ class Minicode < Formula
   license "MIT"
 
   bottle do
+    root_url "https://github.com/nicolaric/minicode/releases/download/v0.1.4"
     sha256 cellar: :any_skip_relocation, arm64_sonoma: "e2cedc3ba9d486d7bea2663f398af5ffbd5b97e1ce91e5d5fdaaac21fc020d52"
     sha256 cellar: :any_skip_relocation, x86_64_linux: "0f088e99cdbb74244056da0bc91e87af4c7c55ce31c7cd5a4b78842be46b1ce4"
   end
